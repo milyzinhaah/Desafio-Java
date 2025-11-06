783604ffb6a5446da22ca157cf9adc2c5d6196fe
milyzinhaah <Amorim.hemily@escola.pr.gov.br>
2025-11-05 21:12:45 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,10 @@
+//Console.log("Meu primeiro programa: trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+    let idade = 18;
+console.log("idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+    let idadeSomada = 20;
+console.log(idadeSomada)
